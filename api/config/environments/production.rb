5e2bb5b3852e6b32393180e07f54d2879526b00b
Robin Hamill <hamillrobin@gmail.com>
2018-01-09 14:24:55 -0500

    Add Devise gem Basic setup

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.action_mailer.default_url_options = { host: 'http://www.dealdropp.com' }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
