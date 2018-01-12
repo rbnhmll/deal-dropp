f6af51e366ca2e5e45df4435129978fbf76fbf04
Robin Hamill <hamillrobin@gmail.com>
2018-01-12 16:18:34 -0500

    Style Changes

--- client/src/components/CardGallery.vue
+++ client/src/components/CardGallery.vue
@@ -19,6 +19,10 @@ export default {
 };
 </script>
 
-<style scoped>
-
+<style lang="sass" scoped>
+  ul
+    margin: 0
+    padding: 0
+    display: flex
+    justify-content: space-between
 </style>
