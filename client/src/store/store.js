354650d18a25bde762ca84c403499f97c53bfe41
Robin Hamill <hamillrobin@gmail.com>
2018-01-12 15:47:56 -0500

    Expand example data

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,8 +5,86 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    searchResults: [],
-    watchItems: [],
+    searchResults: [
+      {
+        name: 'A book',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '7.99',
+      },
+      {
+        name: 'A movie',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '9.99',
+      },
+      {
+        name: 'A horse',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '10900.99',
+      },
+    ],
+    watchItems: [
+      {
+        name: 'Amazon Alexa',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '129.99',
+      },
+      {
+        name: 'Amazon Fire',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '99.99',
+      },
+      {
+        name: 'Amazon Echo',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '109.99',
+      },
+    ],
+    popularItems: [
+      {
+        name: 'Amazon Alexa',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '129.99',
+      },
+      {
+        name: 'Amazon Fire',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '99.99',
+      },
+      {
+        name: 'Amazon Echo',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '109.99',
+      },
+    ],
+    recentDropps: [
+      {
+        name: 'Amazon Alexa',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '129.99',
+      },
+      {
+        name: 'Amazon Fire',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '99.99',
+      },
+      {
+        name: 'Amazon Echo',
+        url:
+          'https://www.amazon.ca/Echo-2nd-Generation-Charcoal-Fabric/dp/B0749ZSPN7/ref=sr_1_1?ie=UTF8&qid=1515769286&sr=8-1&keywords=alexa',
+        price: '109.99',
+      },
+    ],
   },
   getters: {},
   mutations: {},
