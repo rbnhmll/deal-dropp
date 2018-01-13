d54d75f3eb7b5b3b7ffad91700449e3886264daf
Robin Hamill <hamillrobin@gmail.com>
2018-01-12 21:02:11 -0500

    Add params to Store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,6 +5,10 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
+    searchParams: {
+      query: '',
+      providers: [],
+    },
     searchResults: [
       {
         name: 'A book',
